112f49a4902aa697cb0fb887afc275a773224a39
Ojasvi <Ojasvisethi2407@gmail.com>
2024-02-13 21:02:01 +0530

    Initial Commit

---
+++ posture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  posture
+//
+//  Created by Ojasvi Sethi on 13/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
